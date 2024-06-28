7bfec5ed5f4ca2dcfb999951b5f824f1e8f3c615
Ethan <81447958+soulfire-singularity@users.noreply.github.com>
2024-06-28 14:48:07 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   AOS.init();
-)};
   
-new Typed('#typed', {
-strings: ["Ethan Norman"],
-typeSpeed: 100,
-backSpeed: 25,
-loop: false,
-showCursor: false
+  new Typed('#typed', {
+    strings: ["Ethan Norman"],
+    typeSpeed: 100,
+    backSpeed: 25,
+    loop: false,
+    showCursor: false
+  });
+  
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    const name = $('#name').val();
+    const email = $('#email').val();
+    const message = $('#message').val();
+    alert(`Thank you, ${name}! Your message has been received.`);
+    // You can add additional form handling here
+  });
 });
