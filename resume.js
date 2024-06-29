352f3fd8238aaac9dd2209ec0f73ee884984a245
soulfire-singularity <ethannorman83@gmail.com>
2024-06-29 10:31:21 -0500

    fsf

--- resume.js
+++ resume.js
@@ -8,13 +8,4 @@ $(document).ready(function() {
     loop: false,
     showCursor: false
   });
-  
-  $('form').on('submit', function(event) {
-    event.preventDefault();
-    const name = $('#name').val();
-    const email = $('#email').val();
-    const message = $('#message').val();
-    alert(`Thank you, ${name}! Your message has been received.`);
-    // You can add additional form handling here
-  });
 });
